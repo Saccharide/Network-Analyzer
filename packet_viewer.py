ca8786c1af43ff9774cfb9781800023c7c2a79be
Saccharide <rtang@email.wm.edu>
2018-04-01 18:31:54 -0700

    Added the function to parse IPv4 address and calculate the header length with version

--- packet_viewer.py
+++ packet_viewer.py
@@ -26,6 +26,7 @@ def main():
         print("Destination: {}, Source: {}, Protocol: {}".format(dest_mac, src_mac, eth_proto))
 
 
+
 # Unpack ethernet frame, return the destination mac addr, source mac addr, and type (IPV4, IPV6, ARP Request/Response)
 def ethernet_frame(data):
     
@@ -40,4 +41,22 @@ def get_mac_addr(bytes_addr):
 
 
 
+# Unpack IPv4 packet
+def ipv4_packet(data):
+    version_with_header_length = data[0]
+
+    # Get pure version number by right shifts 4 bits
+    version = version_with_header_length >> 4
+
+    # Get pure header length by left shifts 4 bits
+    header_length = (version_with_header_length & 15) * 4
+
+    time_to_live, proto, scr, dest = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+
+    return version, header_length, time_to_live, proto, ipv4(src), ipv4(dest), data[header_length:]
+
+
+# Returns human readabel IPv4 address
+def ipv4(addr):
+    return '.'.join(map(str, addr))
 main()
